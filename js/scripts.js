55e2ea105007eb96230d1bea42827c726423c839
mhayes123 <mhayes30@comcast.net>
2022-07-20 22:30:20 -0700

    Made revisions in order for add button to work properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,73 @@
-function newItem() {
+let todoList = (function () {
+
+  let textarea = $('textarea');
+  let addButton = $('#button');
+  let map = {Enter: false, Control: false};
+
+  function newItem() {
 
     let li = $('<li></li>');
-    let inputValue = $('input').val();
-    li.append(inputValue);
-  
-    if (inputValue === '') {
+    let textareaValue = textarea.val();
+    li.append(textareaValue);
+
+    if (textareaValue === '') {
       alert("You must write something!");
     } else {
       $('#list').append(li);
     }
-  
+
     function crossOut() {
       li.toggleClass("strike");
     }
-  
+
     li.on("dblclick", crossOut);
-  
+
     let crossOutButton = $('<crossOutButton></crossOutButton>');
     crossOutButton.append("X");
+    
+    if (li.css('background')[5] === '0') {
+      crossOutButton.addClass('grey-hover');
+    }
     li.append(crossOutButton);
-  
+
     crossOutButton.on("click", deleteListItem);
     function deleteListItem() {
-      li.addClass("delete")
+      li.fadeOut(250);
     }
     $('#list').sortable();
-  
   }
+
+  textarea.on('keydown', e => {
+    if (e.key in map) {
+      map[e.key] = true;
+    }
+    
+    if (Object.values(map)
+    .filter(v => v === true)
+    .length === 2) {
+      newItem();
+    }
+  });
+
+  textarea.on('keyup', e => {
+    if (Object.values(map)
+    .filter(v => v === true)
+    .length === 2) {
+      textarea.val('');
+    }
+
+    if (e.key in map) {
+      map[e.key] = false;
+    }
+  });
+
+  addButton.on('click', e => {
+    textarea.val('');
+    textarea.focus();
+  })
+
+  return {
+    newItem: newItem
+  };
+
+})();
